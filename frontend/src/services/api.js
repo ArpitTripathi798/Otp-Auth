7602ed9849a6ac94c094c00c8628ab8e0ec42327
Arpit Tripathi <arpit786tripathi@gmail.com>
2026-01-18 11:43:05 +0530

    Fix signup API route path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL
+const api = axios.create({
+  baseURL: "https://otp-auth-ensn.onrender.com/api",
 });
 
-export default API;
+export default api;
+
 
